58e65f0b790271b8fbbe6f72eda8ca011fdc1057
Samuel <valero@compsaonline.com>
2024-02-09 19:21:16 +0100

    Migrations of last change.

--- database/migrations/create_tickets_table.php
+++ database/migrations/create_tickets_table.php
@@ -13,18 +13,31 @@ return new class extends Migration
             $table->string('identifier', 10)->unique();
             $table->string('ticketable_type')->nullable();
             $table->bigInteger('ticketable_id')->nullable();
-            $table->uuid('user_id');
-            $table->uuid('assigned_to_id')->nullable();
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->unsignedBigInteger('user_id');
             $table->tinyInteger('status')->default(0);
             $table->tinyInteger('priority')->default(0);
             $table->string('title');
             $table->text('content');
             $table->timestamps();
+            $table->foreign('category_id')->references('id')->on('categories')->cascadeOnUpdate()->nullOnDelete();
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
         });
+
+        Schema::create('ticket_user', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('ticket_id');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+            $table->foreign('ticket_id')->references('id')->on('tickets')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
+        });
+
     }
 
     public function down()
     {
+        Schema::dropIfExists('ticket_user');
         Schema::dropIfExists('tickets');
     }
 };
